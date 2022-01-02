56a73122c3c2f54288be9f7a7ab255550d97d8b3
Abdelrahman Sadawy <asadawy110@gmail.com>
2022-01-02 20:59:45 +0200

    modification

--- src/sprint1/Discount.java
+++ src/sprint1/Discount.java
@@ -7,37 +7,46 @@ public class Discount {
 
     final double discountPercentage10 = 0.1;
     final double discountPercentage5 = 0.05;
-
+    
+    public void upgradeBalance(Ride ride, double discountPercentage){
+        ride.getCustomer().balance += discountPercentage * ride.getPrice();
+    }
+    
     public void discountAreas(ArrayList<String> discountAreas, Ride ride) {
         if (discountAreas.contains(ride.destination)) {
-            ride.getCustomer().balance += discountPercentage10 * ride.getPrice();
+            upgradeBalance(ride,  discountPercentage10);
+            System.out.println("area");
         }
     }
 
     public void discountTwoPassengers(Ride ride) {
         if (ride.numOfPassengers >= 2) {
-            ride.getCustomer().balance += discountPercentage5 * ride.getPrice();
+            upgradeBalance(ride, discountPercentage5);
+            System.out.println("pass");
         }
         
     }
 
     public void firstRideDiscount(Ride ride) {
         if (ride.getCustomer().Rides.isEmpty()) {
-            ride.getCustomer().balance += discountPercentage10 * ride.getPrice();
+            upgradeBalance(ride,  discountPercentage10);
+            System.out.println("ride");
         }
     }
 
     public void birthDayDiscount(Ride ride) {
         LocalDate date = LocalDate.parse(ride.getCustomer().bithDate);
         if (date.getDayOfMonth() == LocalDate.now().getDayOfMonth() && date.getMonthValue() == LocalDate.now().getMonthValue()) {
-            ride.getCustomer().balance += discountPercentage10 * ride.getPrice();
+            upgradeBalance(ride,  discountPercentage10);
+            System.out.println("birth");
         }
     }
 
     public void holidaysDiscount(ArrayList<LocalDate> publicHolidays, Ride ride) {
         for (LocalDate date : publicHolidays) {
             if (date.getDayOfMonth() == LocalDate.now().getDayOfMonth() && date.getMonthValue() == LocalDate.now().getMonthValue()) {
-                ride.getCustomer().balance += discountPercentage5 * ride.getPrice();
+                upgradeBalance(ride, discountPercentage5);
+                System.out.println("holi");
                 break;
             }
         }
